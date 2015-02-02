63c3dcfae65499639144ca789bebda3fe3881863
laralynmowers <lmowers@gmail.com>
2015-02-02 04:56:11 -0500

    add ruby exercises 5-9

---
+++ ex5.rb
@@ -0,0 +1,21 @@
+name = 'Laralyn Mowers'
+age = 35 #not a lie in 2015
+height = 66 #inches
+weight = 110 #pounds
+eyes = 'Grey'
+teeth = 'White'
+hair = 'Wild, Untamed'
+
+
+puts "Let's talk about #{name}."
+puts "She is #{height} inches tall."
+puts "She weighs #{weight} pounds."
+puts "She's got #{eyes} eyes and #{hair} hair."
+puts "Her teeth are usually #{teeth}, depending on how much coffee she's had."
+
+# This calculation will only work if all variables are numbers. It won't work if there's a string in there.
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+puts "This year, I'll be #{age + 1}."
+puts "Her mass is #{height * weight} <some unit of measurement>."
+
+
