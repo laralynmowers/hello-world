99cc48c1cb71900da46bf0a60d4043aaa4d05d8e
laralynmowers <lmowers@gmail.com>
2015-02-02 05:01:58 -0500

    some text changes to exercise 5

--- ex5.rb
+++ ex5.rb
@@ -1,21 +1,20 @@
-name = 'Laralyn Mowers'
+name = 'Lar'
 age = 35 #not a lie in 2015
 height = 66 #inches
 weight = 110 #pounds
 eyes = 'Grey'
 teeth = 'White'
-hair = 'Wild, Untamed'
+hair = 'Wild, Untamed, Big'
 
 
 puts "Let's talk about #{name}."
-puts "She is #{height} inches tall."
+puts "She's #{height} inches tall."
 puts "She weighs #{weight} pounds."
 puts "She's got #{eyes} eyes and #{hair} hair."
-puts "Her teeth are usually #{teeth}, depending on how much coffee she's had."
+puts "Her teeth are usually #{teeth}——depending on how much coffee she's had."
 
 # This calculation will only work if all variables are numbers. It won't work if there's a string in there.
 puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
-puts "This year, I'll be #{age + 1}."
-puts "Her mass is #{height * weight} <some unit of measurement>."
-
+puts "This year, #{name} be #{age + 1}."
+puts "If she got more winter riding in, she'd probs weigh #{weight + 10}, and the difference would be muscle."
 
