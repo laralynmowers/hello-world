5efc408a3a91657f79869896b865a25fc4d14f4c
laralynmowers <lm2725@caa.columbia.edu>
2015-02-04 17:03:40 -0500

    Revert "Revert "Added a comment""
    This reverts commit 2035c6640b2df405980384525d3cb3e746c6efc1.

--- hello.rb
+++ hello.rb
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 # This is an unwanted, but committed, change.
 # Default is "World"
-=======
-
->>>>>>> parent of 295c729... Added a comment
 name = ARGV.first || "World"
 
 puts "Hello, #{name}!"
