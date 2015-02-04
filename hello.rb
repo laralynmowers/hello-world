2035c6640b2df405980384525d3cb3e746c6efc1
laralynmowers <lm2725@caa.columbia.edu>
2015-02-04 17:01:21 -0500

    Revert "Added a comment"
    This reverts commit 295c7292845998d094e6577e581a3cb807cf7474.
    
    Conflicts:
    	hello.rb

--- hello.rb
+++ hello.rb
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 # This is an unwanted, but committed, change.
 # Default is "World"
+=======
+
+>>>>>>> parent of 295c729... Added a comment
 name = ARGV.first || "World"
 
 puts "Hello, #{name}!"
