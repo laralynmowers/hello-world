2ec6017923c5f3f985f515fd0327177b30d21e97
laralynmowers <lm2725@caa.columbia.edu>
2015-02-05 02:25:54 -0500

    Escapes sequences

---
+++ ex9.rb
@@ -0,0 +1,11 @@
+puts "These are various escape sequences. By inserting a backslash (\\) before these characters, the character shows up."
+puts """
+\\\\  enters a single backslash
+\\\'  enters a single quote
+\\\"  enters a double quote
+\\V  (lower case) vertical tab
+\\A  (lower case) makes the annoying asci bell
+\\b  asci backspace
+
+"""
+
